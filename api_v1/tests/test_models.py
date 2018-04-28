47c324900592949d6bf04f30b5a5470b7bc1faf6
Alex Kiura <kiuraalex@gmail.com>
2018-04-29 01:12:38 +0300

    [Feature] Test BankingUser

--- api_v1/tests/test_models.py
+++ api_v1/tests/test_models.py
@@ -1,3 +1,32 @@
 from django.test import TestCase  # noqa: F401
+from ..models import BankingUser
+from django.db import IntegrityError
 
-# Create your tests here.
+
+class BankingUserTestCase(TestCase):
+    def setUp(self):
+        self.test_user = BankingUser.objects.create_user(
+            email='test_user@example.com')
+
+    def test_creating_banking_user(self):
+        current_users = BankingUser.objects.count()
+        BankingUser.objects.create_user(email='new_user@example.com')
+        self.assertEqual(BankingUser.objects.count(), current_users + 1)
+        new_user = BankingUser.objects.last()
+        self.assertEqual(new_user.email, 'new_user@example.com')
+
+    def test_can_not_create_duplicates(self):
+        BankingUser.objects.create_user(email='new_user@example.com')
+        with self.assertRaises(IntegrityError):
+            BankingUser.objects.create_user(email='new_user@example.com')
+
+    def test_banking_user_verification(self):
+        """
+        Tests the verification of a user by taking an unverified_user and
+        verifying them.
+        """
+        self.assertTrue(self.test_user.is_verified)
+        self.test_user.verify()
+        verified_user = BankingUser.objects.create_user(
+            email='test_user@example.com')
+        self.assertTrue(verified_user.is_verified)
