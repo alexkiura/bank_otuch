24c93477383f747d0796b81f0fa1e24c5e7b7f5a
Alex Kiura <kiuraalex@gmail.com>
2018-04-29 16:13:26 +0300

    [Fix] Fix user serializer

--- api_v1/serializers.py
+++ api_v1/serializers.py
@@ -7,16 +7,20 @@ from .models import BankingUser
 
 class BankingUserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=255, required=True)
-    national_id = serializers.EmailField(max_length=100, required=True)
+    national_id = serializers.CharField(max_length=100, required=True)
+    first_name = serializers.CharField(max_length=100, required=True)
+    last_name = serializers.CharField(max_length=100, required=True)
     date_of_birth = serializers.DateField(required=True)
 
     def validate(self, data):
         try:
             validate_email(data['email'])
+            super().validate(data)
             return data
-        except ValidationError:
-            raise serializers.ValidationError('The email is invalid.')
+        except ValidationError as error:
+            raise serializers.ValidationError(error.message)
 
     class Meta:
         model = BankingUser
-        fields = ('date_of_birth', 'email', 'national_id')
+        fields = ('date_of_birth', 'email', 'national_id', 'first_name',
+                  'last_name')
