306bc7e6d5d8ebe04a484893a98ab5e3244f06d3
Alex Kiura <kiuraalex@gmail.com>
2018-04-29 03:33:38 +0300

    [Chore] update test settings to use default apps

--- bank/settings/test.py
+++ bank/settings/test.py
@@ -5,19 +5,6 @@ from .base import *  # noqa: F403,F401
 
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
-PREREQ_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
-
-PROJECT_APPS = []
-
-INSTALLED_APPS = PREREQ_APPS + PROJECT_APPS
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
