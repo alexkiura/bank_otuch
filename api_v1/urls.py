32eefc241933e99f54449efcd022eb517a1ce7b6
Alex Kiura <kiuraalex@gmail.com>
2018-04-30 12:01:38 +0300

    [Feature] Add bank Account views

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework import routers
 
-from .views import BankingUserCreateViewSet, BankingUserVerifyViewSet
+from .views import (BankingUserCreateViewSet, BankingUserVerifyViewSet,
+                    BankAccountViewSet)
+
+router = routers.SimpleRouter()
+router.register('account', BankAccountViewSet)
 
 urlpatterns = [
     path(
@@ -14,5 +19,12 @@ urlpatterns = [
         BankingUserVerifyViewSet.as_view({'post': 'verify'}),
         name='user-verify'
     ),
-    path('auth/login/', obtain_jwt_token, name='user-verify')
+    path('auth/login/', obtain_jwt_token, name='user-verify'),
+    path('account/', BankAccountViewSet.as_view({
+        'post': 'create',
+        'get': 'list'})),
+    path('account/<int:pk>/', BankAccountViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'}))
 ]
