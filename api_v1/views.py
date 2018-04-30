b3d3ff12119287803092c0d3dd47c878c15a935f
Alex Kiura <kiuraalex@gmail.com>
2018-04-30 15:11:39 +0300

    [Feature] Add transaction views

--- api_v1/views.py
+++ api_v1/views.py
@@ -3,9 +3,9 @@ from rest_framework import status, viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from .models import BankingUser, BankAccount
+from .models import BankingUser, BankAccount, Transaction
 from .serializers import (BankingUserSerializer, BankingUserVerifySerializer,
-                          BankAccountSerializer)
+                          BankAccountSerializer, TransactionSerializer)
 
 
 class BankingUserCreateViewSet(viewsets.ModelViewSet):
@@ -84,3 +84,41 @@ class BankAccountViewSet(viewsets.ModelViewSet):
     queryset = BankAccount.objects.all()
     serializer_class = BankAccountSerializer
     permission_classes = (IsAuthenticated,)
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def list(self, request):
+        transactions = Transaction.objects.filter(
+            account__owner=request.user).all()
+        serializer = self.get_serializer(transactions, many=True)
+        return Response(serializer.data)
+
+    def do_transact(self, request):
+        amount = request.data.get('amount')
+        transaction_type = request.data.get('transaction_type')
+        account_id = request.data.get('account')
+        description = request.data.get('description')
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({'error':
+                             serializer.errors},
+                            status=status.HTTP_400_BAD_REQUEST)
+        try:
+            account = BankAccount.objects.get(id=account_id)
+            transaction = Transaction.objects.create(
+                amount=amount,
+                transaction_type=transaction_type,
+                account=account,
+                description=description
+            )
+            serializer = TransactionSerializer(transaction)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as error:
+            return Response({
+                'error': [error.args]},
+                status=status.HTTP_400_BAD_REQUEST
+            )
